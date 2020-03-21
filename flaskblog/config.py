12c2502e982039a59541153eef5ba34a99a611b7
mexxbro <thomasbroersen@hotmail.com>
2020-03-21 16:04:37 +0100

    first commit

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = 'ccd2d51014dc3a20e0ac6ebf3b0ecd4d'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.office365.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'bim11-15@hotmail.com'
+    MAIL_PASSWORD = ''
